89551893ee55dff1e238cd83218ffcf70276f44a
Anna Volovik <anna_volovik@hotmail.com>
2021-10-28 10:54:41 +0100

    GoogleMapFunctionComponent

--- src/components/GoogleMapFunctionComponent.js
+++ src/components/GoogleMapFunctionComponent.js
@@ -1,21 +1,18 @@
-/*eslint-disable */
-import React, { useState } from 'react'
+import React from 'react'
 import GoogleMapReact from 'google-map-react'
 import { mapStyles } from '../assets/MapStyles'
 
-const handleApiLoaded = (map, maps, marker) => {
-  map.addListener('click', function (e) {
-    placeMarker(e.latLng, map)
-  })
 
-  function placeMarker(position, map, lat, lng) {
-    const myLatlng = { lat: 59.95, lng: 30.33 }
+const placeMarker = (position, map, lat, lng) => {
 
-    var marker = new google.maps.Marker({
-      position: position,
-      map: map,
-      lat: lat,
-      lng: lng,
+  const myLatlng = { lat: position.lat(), lng: position.lng() }
+
+    /* eslint-disable-next-line */
+    const newMarker = new google.maps.Marker({
+      position,
+      map,
+      lat,
+      lng,
     })
 
     const contentString = `<div id="content">
@@ -27,26 +24,36 @@ const handleApiLoaded = (map, maps, marker) => {
     </div>
     </div>`
 
+    /* eslint-disable-next-line */
     const infoWindow = new google.maps.InfoWindow({
       content: contentString,
-      position: position,
+      position,
     })
 
-    marker.addListener('click', () => {
-      infoWindow.open({
-        anchor: marker,
+    infoWindow.open({
+        anchor: newMarker,
         map,
         shouldFocus: false,
       })
-    })
+
+    // newMarker.addListener('click', () => {
+    //   infoWindow.open({
+    //     anchor: newMarker,
+    //     map,
+    //     shouldFocus: false,
+    //   })
+    // })
 
     map.panTo(position)
   }
-}
 
-// style as pop up
-const AnyReactComponent = ({ coords, name }) =>
-  <div style={{backgroundColor: "#ffffff", padding: "20px", width: "200px", height: "200px" }}>{name} {coords.lat} {coords.lng}</div>
+const handleApiLoaded = (mapInstance, maps, marker) => {
+
+  mapInstance.addListener('click', (e) => {
+    placeMarker(e.latLng, mapInstance, marker)
+  })
+
+}
 
 const createMapOptions = () =>  ({
   scrollwheel: false,
@@ -56,12 +63,6 @@ const createMapOptions = () =>  ({
 
 const SimpleMap = () => {
 
-  const [coords, setCoords] = useState({
-    lat: 59.95,
-    lng: 30.33
-  })
-
-
   const defaultProps = {
     center: {
       lat: 59.95,
@@ -70,32 +71,21 @@ const SimpleMap = () => {
     zoom: 11,
   }
 
-  const _onClick = (obj) => {
-    setCoords({
-      lat: obj.lat,
-      lng: obj.lng
-    })
-  }
-
   return (
     <div
       className="container"
       id="map"
       style={{ height: '100vh', width: '100%' }}
     >
-      {/*<div style={{backgroundColor: "#ffffff", width: "200px", height: "200px", position: "absolute", zIndex: 99, top: "50%", left: "50%"}}>Pop up</div>*/}
       <GoogleMapReact
         bootstrapURLKeys={{ key: 'AIzaSyDZ-G11woEVuWi_wkX6j77pP2tqPe_5lVY' }}
-        defaultCenter={coords}
+        defaultCenter={defaultProps.center}
         defaultZoom={defaultProps.zoom}
-        yesIWantToUseGoogleMapApiInternals={true}
+        yesIWantToUseGoogleMapApiInternals
         onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
-        // styles={mapStyles}
         options={createMapOptions}
-        onClick={_onClick}
-      >
-        <AnyReactComponent coords={coords} name="Custom location"/>
-      </GoogleMapReact>
+      />
+
     </div>
   )
 
