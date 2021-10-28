cbd2d0af4b75e7d88c237d045132a68d042893ec
M.J. Nicholls <marknicholls@openweather.co.uk>
2021-10-28 15:33:32 +0100

    map 4

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import GoogleMapReact from 'google-map-react'
 import '../App.scss'
 import {
   Col,
@@ -12,6 +13,18 @@ import {
   Label,
 } from 'reactstrap'
 
+
+function initialize() {
+    const input = document.getElementById('searchTextField');
+     /* eslint-disable-next-line */
+    new google.maps.places.Autocomplete(input);
+  }
+   /* eslint-disable-next-line */
+  google.maps.event.addDomListener(window, 'load', initialize);
+
+ /* eslint-disable-next-line */
+//const autocomplete = new google.maps.places.Autocomplete(input, options);
+
 const SearchBox = () => (
   <Col md="7">
     <Row>
@@ -24,14 +37,20 @@ const SearchBox = () => (
             <Form>
               <FormGroup>
                 <Label>Name</Label>
-                <Input className="input" type="text" />
+                <Input className="input" type="text" id="pac-input" />
               </FormGroup>
             </Form>
           </CardHeader>
         </Card>
       </Col>
     </Row>
+    <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyDZ-G11woEVuWi_wkX6j77pP2tqPe_5lVY' }}
+          yesIWantToUseGoogleMapApiInternals
+        
+        />
   </Col>
+  
 )
 
 export default SearchBox
