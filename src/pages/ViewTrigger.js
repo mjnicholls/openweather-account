aa93604da364619a208332934fb2674410405e34
M.J. Nicholls <marknicholls@openweather.co.uk>
2021-11-08 15:00:16 +0000

    trigger card

--- src/pages/ViewTrigger.js
+++ src/pages/ViewTrigger.js
@@ -1,7 +1,17 @@
 import React, { useRef, useState } from 'react'
-import { Row, Col, Input, Label, Button } from 'reactstrap'
+import {
+  Row,
+  Col,
+  Input,
+  Label,
+  Button,
+} from 'reactstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPenSquare, faThumbsUp } from '@fortawesome/free-solid-svg-icons'
+import {
+  faArrowDown,
+  faPenSquare,
+  faThumbsUp,
+} from '@fortawesome/free-solid-svg-icons'
 import humanReadableCondition from '../humanReadableCondition'
 import MyMapComponent from '../components/GoogleMapCreate'
 import '../App.scss'
@@ -9,6 +19,7 @@ import '../App.scss'
 const noBlankErrorMessage = 'Cannot be blank'
 
 const ViewTrigger = (props) => {
+
   const mapRef = useRef(null)
   const [locations, setLocation] = useState('')
   const [error, setError] = useState('')
@@ -34,172 +45,188 @@ const ViewTrigger = (props) => {
   }
 
   const saveName = () => {
-    const nameSet = activeNameContent
+    const nameSet = activeNameContent;
     validationName(name)
     setActiveName('')
     setActiveNameContent(nameSet)
   }
 
-  const data = {
-    id: 1,
-    condition: {
-      condition: '>',
-      units: 'metric',
-      value: 20,
-      variable: 'temp',
-    },
-    days: 3,
-    name: 'Trigger 1',
-    recipients: ['email1', 'email2', 'email3'] || {},
-    status: false,
-    location: {
-      name: 'Paris',
-      lat: 40.4,
-      lon: 28.8,
-    },
-    user_id: 1,
-  }
 
-  const { condition, days, id, location, name, recipients, status } = data
+
+  const data = {
+      id: 1,
+      condition: {
+        condition: '>',
+        units: 'metric',
+        value: 20,
+        variable: 'temp',
+      },
+      days: 3,
+      name: 'Trigger 1',
+      recipients: ['email1', 'email2', 'email3'] || {},
+      status: false,
+      location: {
+        name: 'Paris',
+        lat: 40.4,
+        lon: 28.8,
+      },
+      user_id: 1,
+    }
+
+
+  const {condition, days, id, location, name, recipients, status} = data
+  
 
   return (
     <>
-      <Row>
-        <Col md="7">
-          <Row>
-            {name === activeName ? (
-              <Row>
-                <Col md="4">
-                  <h3>Trigger Card</h3>
-                </Col>
+    <Row>
+      <Col md="7">
+    <Row>
+    {name === activeName ? (
+        
+            <Row>
+              <Col md="4"><h3>Trigger Card</h3></Col>
 
-                <Col md="4" key={name}>
-                  <Input
-                    type="text"
-                    onChange={(e) => {
-                      setActiveNameContent(e.target.value)
-                    }}
-                    className={error.name ? 'danger-border' : ''}
-                    value={activeName}
-                    name="name"
-                  />
-                </Col>
-                <Col md="1" className="icons">
-                  <FontAwesomeIcon
-                    icon={faThumbsUp}
-                    onClick={() => saveName()}
-                  />
-                </Col>
-                <Col md="2">
-                  {status === true ? (
-                    <Button>Active</Button>
-                  ) : (
-                    <Button>Deactivated</Button>
-                  )}
-                </Col>
-              </Row>
-            ) : (
-              <Row>
-                <Col md="4">
-                  <h3>Trigger Card</h3>
-                </Col>
-                <Col md="4" key={name}>
-                  <p>{name}</p>
-                </Col>
-                <Col md="1" className="icons">
-                  <FontAwesomeIcon
-                    icon={faPenSquare}
-                    onClick={() => {
-                      setActiveName(name)
-                      setActiveNameContent(name)
-                    }}
-                  />
-                </Col>
-                <Col md="2">
-                  <Button>Active</Button>
-                </Col>
-              </Row>
-            )}
-          </Row>
-          <Row className="search-box">
-            <Col md="3">
-              <Label>Location</Label>
-            </Col>
-
-            <Col md="9">
-              <Label type="text" value={location} className="cardContent">
-                {location.name} ({location.lat}, {location.lon})
-              </Label>
-            </Col>
-          </Row>
-          <Row className="search-box">
-            <Col md="3">
-              <Label>Trigger Condition</Label>
-            </Col>
-
-            <Col md="9">
-              <Label type="text" value={condition} className="cardContent">
-                {humanReadableCondition(condition).substring(27)}
-              </Label>
-            </Col>
-          </Row>
-          <Row className="search-box">
-            <Col md="3">
-              <Label>Email recipients</Label>
-            </Col>
-
-            <Col md="9">
-              <Label type="text" className="cardContent">
-                <p>{recipients[0]}</p>
-                <p>{recipients[1]}</p>
-                <p>{recipients[2]}</p>
-              </Label>
-            </Col>
-          </Row>
-          <Row>
-            <h3>Events List</h3>
-          </Row>
-          <Row className="search-box">
-            <Col md="3">
-              <Label>Active Events</Label>
-            </Col>
-
-            <Col md="9">
-              <Label type="text" value={recipients} className="cardContent">
-                27 October 2021
-              </Label>
-            </Col>
-          </Row>
-          <Row className="search-box">
-            <Col md="3">
-              <Label>Archive</Label>
-            </Col>
-
-            <Col md="9">
-              <Label type="text" value={recipients} className="cardContent">
-                27 September 2021
-              </Label>
-            </Col>
-          </Row>
-          <Row className="search-box">
-            <Col md="7"></Col>
-            <Col md="5">
-              <Button className="bottom-buttons">Edit</Button>
-
-              <Button className="bottom-buttons">Delete</Button>
-            </Col>
-          </Row>
+              <Col md="4" key={name}>
+                <Input
+                  type="text"
+                  onChange={(e) => {
+                    setActiveNameContent(e.target.value)
+                  }}
+                  className={error.name ? 'danger-border' : ''}
+                  value={activeName}
+                  name="name"
+                />
+              </Col>
+              <Col md="1" className="icons">
+                <FontAwesomeIcon
+                  icon={faThumbsUp}
+                  onClick={() => saveName()}
+                />
+              </Col>
+              <Col md="2">
+                {status === true ? (
+        <Button>
+          Active
+        </Button>
+                ) : (
+                  <Button>
+                  Deactivated
+                </Button>
+                )}
+      </Col>
+            </Row>
+          
+        ) : (
+    <Row>
+            <Col md="4"><h3>Trigger Card</h3></Col>
+              <Col md="4" key={name}>
+              <p>{name}</p>
+              </Col>
+              <Col md="1" className="icons">
+                <FontAwesomeIcon
+                  icon={faPenSquare}
+                  onClick={() => {
+                    setActiveName(name)
+                    setActiveNameContent(name)
+                  }}
+                />
+              </Col>
+              <Col md="2">
+        <Button>
+          Active
+        </Button>
+      </Col>
+          
+            </Row>
+        )}
+    </Row>
+      <Row className="search-box">
+        <Col md="3">
+          <Label>Location</Label>
         </Col>
-        <Col md="5">
+
+        <Col md="9">
+          <Label type="text" value={location} className="cardContent">
+          {location.name} ({location.lat}, {location.lon})
+          </Label>
+        </Col>
+      </Row>
+      <Row className="search-box">
+        <Col md="3">
+          <Label>Trigger Condition</Label>
+        </Col>
+
+        <Col md="9">
+          <Label type="text" value={condition} className="cardContent">
+          {humanReadableCondition(condition).substring(27)}
+          </Label>
+        </Col>
+      </Row>
+      <Row className="search-box">
+        <Col md="3">
+          <Label>Email recipients</Label>
+        </Col>
+
+        <Col md="9">
+          <Label type="text" className="cardContent">
+            <p>{recipients[0]}</p>
+            <p>{recipients[1]}</p>
+            <p>{recipients[2]}</p>
+            <FontAwesomeIcon
+                  icon={faArrowDown}
+                  onClick={() => saveName()}
+                />
+          </Label>
+        </Col>
+      </Row>
+      <Row>
+      <h3>Events List</h3>
+      </Row>
+      <Row className="search-box">
+        <Col md="3">
+          <Label>Active Events</Label>
+        </Col>
+
+        <Col md="9">
+          <Label type="text" value={recipients} className="cardContent">
+         27 October 2021
+         </Label>
+        </Col>
+      </Row>
+      <Row className="search-box">
+        <Col md="3">
+          <Label>Archive</Label>
+        </Col>
+
+        <Col md="9">
+          <Label type="text" value={recipients} className="cardContent">
+         27 September 2021
+         </Label>
+        </Col>
+      </Row>
+      <Row className="search-box">
+              <Col md="7"></Col>
+              <Col md="5">
+                <Button className="bottom-buttons">Edit</Button>
+
+                <Button className="bottom-buttons">
+                 Delete
+                </Button>
+              </Col>
+            </Row>
+      </Col>
+      <Col md="5">
           <MyMapComponent
             mapRef={mapRef}
             location={location}
             setLocation={setLocation}
           />
-        </Col>
+      </Col>
       </Row>
     </>
   )
 }
+
 
 export default ViewTrigger
