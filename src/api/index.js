cbd9a34d95fb6c43d11ab1ed2ca5035a3a18278f
Anna Volovik <anna_volovik@hotmail.com>
2021-11-18 14:02:42 +0000

    base url update

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,4 @@
-const baseURL = 'http://phase.owm.io:2403/'
+const baseURL = process.env.REACT_APP_BASE_URL
 export const getIndexURL = `${baseURL}triggers/v1/trigger/all`
 export const postTriggerURL = `${baseURL}triggers/v1/trigger`
 export const patchTriggerURL = `${baseURL}triggers/v1/trigger`
