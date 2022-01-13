fb862fed01fd979e2a261851c633363c1fa88916
Anna Volovik <anna_volovik@hotmail.com>
2022-01-13 12:04:36 +0000

    Location display

--- src/components/ThumbnailLocation.js
+++ src/components/ThumbnailLocation.js
@@ -5,14 +5,8 @@ import { Location } from 'react-ikonate'
 
 const ThumbnailLocation = ({ location, showIcon = true }) => (
   <div className="thumbnail ps-0">
-    {showIcon && (
-      <div>
-        <Location className="me-1" />
-      </div>
-    )}
-    <span>
-      {location.name} ({location.lat.toFixed(3)}, {location.lon.toFixed(3)})
-    </span>
+    {showIcon && <Location className="me-1" />}
+    <span>{location.name}</span>
   </div>
 )
 
