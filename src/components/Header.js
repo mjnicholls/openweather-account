98da283474cabfcec8b5b01d8c3c58473a0f2bb8
M.J. Nicholls <marknicholls@openweather.co.uk>
2022-01-17 12:41:10 +0000

    updated link

--- src/components/Header.js
+++ src/components/Header.js
@@ -25,30 +25,6 @@ const HeaderTrigger = () => {
             />
           </a>
 
-          {/* <div className="input-group search d-none d-xxl-flex"> */}
-          {/* <div className="input-group-append"> */}
-          {/* <button className="btn btn-secondary" type="button"> */}
-          {/* <img */}
-          {/* src="https://openweathermap.org/themes/openweathermap/assets/img/owm_icons/icon_search.svg" */}
-          {/* alt="search bar logo" */}
-          {/* /> */}
-          {/* </button> */}
-          {/* </div> */}
-          {/* <form */}
-          {/* className="input" */}
-          {/* action="https://openweathermap.org/find" */}
-          {/* acceptCharset="UTF-8" */}
-          {/* method="get" */}
-          {/* > */}
-          {/* <input name="utf8" type="hidden" value="&#x2713;" /> */}
-          {/* <input */}
-          {/* type="text" */}
-          {/* className="form-control" */}
-          {/* placeholder="Weather in your city" */}
-          {/* name="q" */}
-          {/* /> */}
-          {/* </form> */}
-          {/* </div> */}
 
           <button
             className="remove-default-button-style d-xxl-none"
